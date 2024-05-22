297e82aa8bfdffeb451c0116dda907eedc4bd2c0
Stephens\Raihanath <raihanalatheef775@gmail.com>
2024-05-22 14:46:27 +1200

    framer motion installed with animation for mobile navigation added

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -3,6 +3,8 @@ import { useState } from "react"
 import Link from "next/link"
 import Image from "next/image"
 import NavLink from "./navLink"
+import { motion } from "framer-motion"
+
 
 const links = [
     { url:"/", title:"Home" },
@@ -13,8 +15,60 @@ const links = [
 
 const Navbar = () => {
     const [open,setOpen] = useState(false)
+    const topVariants = {
+        closed: {
+            rotate: 0,
+        },
+        opened: {
+            rotate: 45,
+            backgroundColor: "rgb(255,255,255)",
+        }
+    }
+    const centerVariants = {
+        closed: {
+            opacity: 1,
+        },
+        opened: {
+            opacity: 0,
+        }
+    }
+
+    const bottomVariants = {
+        closed: {
+            rotate: 0,
+        },
+        opened: {
+            rotate: -45,
+            backgroundColor: "rgb(255,255,255)", 
+        }
+    }
+
+    const listVariants = {
+        closed: {
+            x: "100vw",
+        },
+        opened: {
+            x: 0,
+            transition: {
+                when: "beforeChildren",
+                staggerChildren: 0.2,
+            }
+        }
+    }
+
+    const listItemVariants = {
+        closed: {
+            x: -10,
+            opacity: 0,
+        },
+        opened: {
+            x: 0,
+            opacity: 1,
+        }
+    }
+
     return (
-        <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 ">
+        <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
             {/* Desktop links/menu */}
             <div className="text-black flex-row items-center justify-start gap-8 md:flex hidden w-1/3">
                 {links.map(linkItem => ( <NavLink link={linkItem}/>))}
@@ -37,17 +91,17 @@ const Navbar = () => {
             </div>
             
             {/* Responsive menu */}
-            <div className="md:hidden">
+            <div className="md:hidden z-10">
                 <button className="flex flex-col justify-between w-10 h-8 z-50 relative" onClick={() => setOpen((prev) => !prev)}>
-                    <div className="bg-black rounded h-1 w-10"></div>
-                    <div className="bg-black rounded h-1 w-10"></div>
-                    <div className="bg-black rounded h-1 w-10"></div>
+                    <motion.div animate={open ? "opened":"closed"} variants={topVariants} className="bg-black rounded h-1 w-10 origin-left"></motion.div>
+                    <motion.div animate={open ? "opened":"closed"} variants={centerVariants} className="bg-black rounded h-1 w-10"></motion.div>
+                    <motion.div animate={open ? "opened":"closed"} variants={bottomVariants} className="bg-black rounded h-1 w-10 origin-left"></motion.div>
                 </button>
                 {/* Menu List */}
                 {open && ( 
-                <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
-                    {links.map(linkItem => ( <Link key={linkItem.url} href={linkItem.url}>{linkItem.title}</Link>))}
-                </div>)}
+                <motion.div variants={listVariants} animate="opened" initial="closed" className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
+                    {links.map(linkItem => (<motion.div variants={listItemVariants} key={linkItem.url}><Link href={linkItem.url}>{linkItem.title}</Link></motion.div>))}
+                </motion.div>)}
             </div>
        
         
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,21 @@
+import Image from "next/image";
+
 const Homepage = () => {
-  return <div>Hi</div>;
+  return (
+  <div className="flex flex-col lg:flex-row h-full lg:gap-8 px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+    <div className="h-1/2 lg:h-full lg:w-1/2 relative">
+    <Image src="/raihanaimage.png" fill alt="Raihana image" className="object-contain"/>
+    </div>
+    <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-start justify-center">
+      <h1 className="text-4xl md:text-6xl font-bold">Front end developer</h1>
+      <p className="md:text-xl">Welcome to my digital canvas</p>
+      <div className="flex gap-4">
+        <button className="p-4 rounded-lg ring-black ring-1 bg-black text-white">View my work</button>
+        <button className="p-4 rounded-lg ring-black ring-1 bg-white text-black">Contact me</button>
+      </div>
+    </div>
+  </div>
+  );
 };
 
 export default Homepage;
